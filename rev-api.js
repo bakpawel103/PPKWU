fe525317410a38001001a240a54b740b0ee7b861
TTIT\bakp <pawel.bak@ttpsc.pl>
2020-10-13 14:56:42 +0200

    Working endpoint ('/rev') that reverse string

--- rev-api.js
+++ rev-api.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
 });
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.post('/rev', (req, res) => {
+    console.log(req.body.stringToRev);
+    res.send(reverse(req.body.stringToRev));
+});
+
+function reverse(s){
+    return [...s].reverse().join("");
+}
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
